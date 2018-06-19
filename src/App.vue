4b711c8779860b48bc04d039ee13472d869e49da
han6054 <424681370@qq.com>
2018-06-20 00:07:21 +0800

    silder

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" @touchmove.prevent>
+    <m-header></m-header>
+    <router-view></router-view>
+    <Tab></Tab>
+  </div>
+</template>
+
+<script>
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app
+    color: $color-theme
+</style>
